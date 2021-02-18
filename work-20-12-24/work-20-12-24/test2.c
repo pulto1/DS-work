0f5ead46950968745fc16539aaa2e9a19a8ac786
pulto <1016828439@qq.com>
2021-02-18 11:39:47 +0800

    判断链表是否有环，返回有环链表环的第一个结点，拷贝含random的链表

---
+++ work-20-12-24/work-20-12-24/test2.c
@@ -0,0 +1,115 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+//给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+//为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 
+//如果 pos 是 - 1，则在该链表中没有环。注意，pos 仅仅是用于标识环的情况，并不会作为参数传递到函数中。
+//
+//说明：不允许修改给定的链表。
+//
+//进阶：
+//你是否可以使用 O(1) 空间解决此题？
+
+//方法1
+/**
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     struct ListNode *next;
+* };
+*/
+struct ListNode *detectCycle(struct ListNode *head)
+{
+	struct ListNode *fast = head;
+	struct ListNode *slow = head;
+
+	while (fast && fast->next)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+
+		if (fast == slow)
+			break;
+	}
+
+	if (fast == NULL || fast->next == NULL)
+		return NULL;
+
+	struct ListNode *meet = fast;
+	while (meet != head)
+	{
+		head = head->next;
+		meet = meet->next;
+	}
+
+	return meet;
+
+}
+
+//方法2
+/**
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     struct ListNode *next;
+* };
+*/
+struct ListNode *detectCycle(struct ListNode *head)
+{
+	struct ListNode *fast = head;
+	struct ListNode *slow = head;
+
+	while (fast && fast->next)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+
+		if (fast == slow)
+			break;
+	}
+
+	if (fast == NULL || fast->next == NULL)
+		return NULL;
+
+	struct ListNode *meetHead = fast->next;
+	fast->next = NULL;
+
+	fast = head;
+	slow = meetHead;
+
+	struct ListNode *curHead = head;
+	int la = 0;
+	while (curHead)
+	{
+		la++;
+		curHead = curHead->next;
+	}
+
+	struct ListNode *curMeetHead = meetHead;
+	int lb = 0;
+	while (curMeetHead)
+	{
+		lb++;
+		curMeetHead = curMeetHead->next;
+	}
+
+	if (la < lb)
+	{
+		fast = meetHead;
+		slow = head;
+	}
+
+	int gap = abs(la - lb);
+
+	while (gap--)
+	{
+		fast = fast->next;
+	}
+
+	while (fast != slow)
+	{
+		fast = fast->next;
+		slow = slow->next;
+	}
+
+	return fast;
+}
---
+++ work-20-12-24/work-20-12-24/test3.c
@@ -0,0 +1,83 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+//给你一个长度为 n 的链表，每个节点包含一个额外增加的随机指针 random ，该指针可以指向链表中的任何节点或空节点。
+//
+//构造这个链表的 深拷贝。 深拷贝应该正好由 n 个 全新 节点组成，其中每个新节点的值都设为其对应的原节点的值。
+//新节点的 next 指针和 random 指针也都应指向复制链表中的新节点，并使原链表和复制链表中的这些指针能够表示相同的链表状态。
+//复制链表中的指针都不应指向原链表中的节点 。
+//
+//例如，如果原链表中有 X 和 Y 两个节点，其中 X.random--> Y 。那么在复制链表中对应的两个节点 x 和 y ，同样有 x.random--> y 。
+//返回复制链表的头节点。
+//用一个由 n 个节点组成的链表来表示输入 / 输出中的链表。每个节点用一个 [val, random_index] 表示：
+//val：一个表示 Node.val 的整数。
+//random_index：随机指针指向的节点索引（范围从 0 到 n - 1）；如果不指向任何节点，则为  null 。
+//你的代码 只 接受原链表的头节点 head 作为传入参数。
+
+/**
+* Definition for a Node.
+* struct Node {
+*     int val;
+*     struct Node *next;
+*     struct Node *random;
+* };
+*/
+
+struct Node* copyRandomList(struct Node* head)
+{
+	//哨兵结点
+	struct Node* newHead = (struct Node*) malloc(sizeof(struct Node));
+	struct Node* curNewList = newHead;
+	struct Node* curList = head;
+
+	//创建新链表，拷贝原链表的val和next指向关系
+	while (curList)
+	{
+		struct Node* newNode = (struct Node*) malloc(sizeof(struct Node));
+		newNode->val = curList->val;
+		curNewList->next = newNode;
+		curNewList = curNewList->next;
+		curList = curList->next;
+	}
+
+	curNewList->next = NULL;
+	struct Node* del = newHead;
+	newHead = newHead->next;
+	free(del);
+
+	//拷贝random指向关系
+	curList = head;
+	curNewList = newHead;
+	while (curList)
+	{
+		struct Node* random = curList->random;
+		if (random == NULL)
+		{
+			curNewList->random = NULL;
+			curNewList = curNewList->next;
+		}
+
+		//原链表的random指向第几个结点，新链表的random就指向第几个结点
+		else
+		{
+			int gap = 0;
+			struct Node* retHead = head;
+			while (retHead != random)
+			{
+				gap++;
+				retHead = retHead->next;
+			}
+
+			struct Node* retNew = newHead;
+			while (gap--)
+			{
+				retNew = retNew->next;
+			}
+
+			curNewList->random = retNew;
+			curNewList = curNewList->next;
+		}
+		curList = curList->next;
+	}
+
+	return newHead;
+}
